bda420e6103a072aa732d7290bbc71032eb46a38
N501YHappy <n501yhappy@outlook.com>
2026-06-30 20:37:07 +0800

    完成标题

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-import './App.css';
-
+import { Typography, useTheme } from "@mui/material";
+import "./App.css";
+const title_list = [
+  { text: "Co", color: "primary" },
+  { text: "la", color: "textPrimary" },
+  { text: "py", color: "primary" },
+  { text: "Yao", color: "textPrimary" },
+];
 function App() {
+  const theme = useTheme();
+  
+  const getColorValue = (colorKey) => {
+    if (colorKey === "primary") {
+      return theme.palette.primary.main;
+    }
+    if (colorKey === "textPrimary") {
+      return theme.palette.text.primary;
+    }
+    return colorKey;
+  };
   return (
     <div className="App">
       <header className="App-header">
-        
+        {title_list.map((item) => (
+          <Typography
+            variant="h1"
+            color={item.color}
+            sx={{
+              fontWeight: "bold",
+              letterSpacing: 6,
+              textShadow: `0 0 10px ${item.color === 'textPrimary' ? null : getColorValue(item.color)}`,
+            }}
+          >
+            {item.text}
+          </Typography>
+        ))}
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const theme = createTheme({
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: "none",
+        },
+      },
+    },
+  },
+});
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
 );
